91584110799fddb87f64102f0cbfb04409b7fae9
Stephan Hoffmann <sho@relinux.de>
2019-07-02 19:49:29 +0200

    Improved startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,9 @@
 
 user_id=$(stat -c '%u' .)
 group_id=$(stat -c '%g' .)
-
-useradd -u $user_id -g $group_id -b /local_home -m local_worker
-exec sudo -H -u local_worker /bin/bash -c "$*"
+if [ "$user_id" = "0" ]; then
+	exec "$@"
+else
+	useradd -u $user_id -g $group_id -b /local_home -m docker_worker
+	exec sudo "PATH=$PATH" -E -H -u docker_worker /bin/bash -c "$*"
+fi
